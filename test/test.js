dcb1469c637cc0370a93ddd1db35649e68aa8e88
talavital7 <talavital1993@gmail.com>
2021-03-11 12:49:38 +0200

    success to build

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ describe('Should Component Contain Object', function() {
   describe('#Pokemon Component', function() {
     it('moreInfo property should be as object', function() {
       var P = new Pokemon()
-      assert.deepEqual(P.state.moreInfo.constructor, string)
+      assert.deepEqual(P.state.moreInfo.constructor, Object)
     })
   })
 })
