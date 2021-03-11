1987b42dab116042339c40f6ad516d217fb51bbc
talavital7 <talavital1993@gmail.com>
2021-03-11 13:25:42 +0200

    commit with heroku

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import Page from './containers/page'
 import './style/main.css'
 import configureStore from './redux/store/configure-store'
-//committtt
+//committtt22
 const store = configureStore()
 
 render(
